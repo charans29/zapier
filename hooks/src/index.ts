28a734773ccae49c6e4c4a7d94a03bc658c991a2
charans29 <smodugula@csuchico.edu>
2024-07-21 20:51:21 -0400

    feat: dvlpd webhooks endpoint

--- hooks/src/index.ts
+++ hooks/src/index.ts
@@ -1,12 +1,32 @@
 import express  from "express";
+import { PrismaClient } from "@prisma/client";
 
 const app = express();
-
 app.use(express.json());
+const client = new PrismaClient();
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
-});
+app.post("/hooks/catch/:userId/:zapId", async (req, res) => {
+    const userId = req.params.userId;
+    const zapId = req.params.zapId;
+    const body = req.body;
+
+    await client.$transaction(async tx => {
+        const run = await tx.zapRun.create({
+            data: {
+                zapId:zapId,
+                metadata:body
+            }
+        })
+        await tx.zapRunOutbox.create({
+            data: {
+                zapRunId:run.id
+            }
+        })
+    })
+    res.json ({
+        message: "webhooks triggered"
+    })
+})
 
 app.listen(3000, () => {
     console.log(`running on http://localhost:3000`);
