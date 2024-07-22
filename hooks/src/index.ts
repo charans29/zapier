e1368fe5f45d735fd1f3b7bf90edc162b228d891
charans29 <smodugula@csuchico.edu>
2024-07-22 18:22:57 -0400

    feat: processor to monorepo

--- hooks/src/index.ts
+++ hooks/src/index.ts
@@ -9,7 +9,7 @@ app.post("/hooks/catch/:userId/:zapId", async (req, res) => {
     const userId = req.params.userId;
     const zapId = req.params.zapId;
     const body = req.body;
-
+    
     await client.$transaction(async tx => {
         const run = await tx.zapRun.create({
             data: {
